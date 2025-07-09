64de6484f889922cce6f2e2a1e9eaa2dc6cebc16
Amadougaye Cissé <amadou_g@hotmail.fr>
2025-07-09 15:25:10 +0200

    Initial Commit

---
+++ LooplayTests/LooplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  LooplayTests.swift
+//  LooplayTests
+//
+//  Created by Amadou on 09.07.2025.
+//
+
+import Testing
+@testable import Looplay
+
+struct LooplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
